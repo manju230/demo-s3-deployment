d1335160b976bb89f962f89512f02a286f7ef4b3
manju230 <111728745+manju230@users.noreply.github.com>
2026-06-09 17:47:09 +0530

    s3-v5

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "s3bucket" {
 bucket = "demo-s3-deployment-01"
 }
 
-resource "aws_s3_bucket" "s3bucket" {
+resource "aws_s3_bucket" "s3bucket1" {
 bucket = "demo-s3-deployment-02"
 }
